2f59b621690bd294d85445fb458a4ca38f066776
Roberts Simanis <robsimanis@gmail.com>
2019-03-02 14:42:54 +0200

    Moved ssh command to a background thread

--- app/src/main/java/com/enelondroid/enhome/MainActivity.java
+++ app/src/main/java/com/enelondroid/enhome/MainActivity.java
@@ -5,13 +5,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.os.AsyncTask;
 
-import com.jcraft.jsch.ChannelExec;
-import com.jcraft.jsch.ChannelSftp;
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.JSchException;
-import com.jcraft.jsch.Session;
-import com.jcraft.jsch.SftpException;
+import com.jcraft.jsch.*;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
@@ -34,34 +30,47 @@ public class MainActivity extends AppCompatActivity {
 
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                executeRemoteCommand();
+
+                new AsyncTaskRunner().execute();
+
+                //executeRemoteCommand();
                 Toast.makeText(MainActivity.this,
                         "Button pressed", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-    public void executeRemoteCommand() {
-        String user = "pi";
-        String password = "raspberry";
-        String host = "192.168.8.111";
-        int port = 22;
+    /*public void executeRemoteCommand() {
 
-        try {
-            JSch jsch = new JSch();
-            Session session = jsch.getSession(user, host, port);
-            session.setPassword(password);
-            session.setConfig("StrictHostKeyChecking", "no");
-            session.connect();
-            // create the execution channel over the session
-            ChannelExec channelExec = (ChannelExec) session.openChannel("exec");
-            // Set the command to execute on the channel and execute the command
-            channelExec.setCommand("cd Desktop && python blink.py");
-            channelExec.connect();
-            session.disconnect();
-        } catch (JSchException e) {
-            e.printStackTrace();
+    }*/
+    private class AsyncTaskRunner extends AsyncTask<String, Void, Void> {
+
+        @Override
+        protected Void doInBackground(String... params) {
+            String user = "pi";
+            String password = "raspi";
+            String host = "192.168.8.111";
+            int port = 22;
+
+            try {
+                JSch jsch = new JSch();
+                Session session = jsch.getSession(user, host, port);
+                session.setPassword(password);
+                session.setConfig("StrictHostKeyChecking", "no");
+                session.connect();
+                // create the execution channel over the session
+                ChannelExec channelExec = (ChannelExec) session.openChannel("exec");
+                // Set the command to execute on the channel and execute the command
+                channelExec.setCommand("cd Desktop && python blink.py");
+                channelExec.connect();
+                session.disconnect();
+            } catch (JSchException e) {
+                e.printStackTrace();
+            }
+            return null;
         }
     }
 }
+
+
 
